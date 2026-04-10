c695aaea1efae6270a90d0ba3f1ecc782a5cfdd8
talsirbu <tals@clover.security>
2026-04-10 23:58:13 +0300

    feat: implement Bottom Navigation, MapFragment and stubs for other fragments

---
+++ app/src/main/java/com/example/playground/ui/profile/ProfileFragment.kt
@@ -0,0 +1,14 @@
+package com.example.playground.ui.profile
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+class ProfileFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val tv = TextView(requireContext())
+        tv.text = "ProfileFragment Placeholder"
+        return tv
+    }
+}
