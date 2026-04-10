4d8a413ec160284aef8cceb3e545b7ada0ff83dd
talsirbu <tals@clover.security>
2026-04-10 23:39:21 +0300

    feat: add Comment entity and DAO

--- app/src/main/java/com/example/playground/data/AppDatabase.kt
+++ app/src/main/java/com/example/playground/data/AppDatabase.kt
@@ -5,10 +5,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class, Event::class], version = 2)
+@Database(entities = [User::class, Event::class, Comment::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
     abstract fun eventDao(): EventDao
+    abstract fun commentDao(): CommentDao
 
     companion object {
         @Volatile
